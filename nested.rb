204c0b31120bb2e6e5743b456432215df952c03d
Christina Howard <clhoward33@icloud.com>
2020-09-14 19:32:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-	programmer_hash = [:alan_kay][:known_for]
+	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
 
-	programmer_hash = [:dennis_ritchie][:languages][0]
+	programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -86,7 +86,7 @@ def adding_matz
 
 
 
- programmer_hash = [:yukihiro_matsumoto] = {}
+ programmer_hash[:yukihiro_matsumoto] = {}
 		 [:yukihiro_matsumoto][:known_for] = "Ruby"
 		 [:yukihiro_matsumoto][:languages] = "LISP", "C"
 		 programmer_hash
@@ -112,7 +112,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash = [:alan_kay][:known_for] = alans_new_info
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 		 programmer_hash
 
 end
@@ -133,7 +133,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-	programmer_hash = [:dennis_ritchie][:languages] = "Assembly"
+	programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 	programmer_hash
 
 end
